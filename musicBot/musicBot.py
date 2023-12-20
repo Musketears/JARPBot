91fa234782351c52d292d2c5b2be604ab2796db0
ryanstack10 <ryanstack10@gmail.com>
2023-12-20 15:42:06 -0500

    adding slots to music bot

--- musicBot/musicBot.py
+++ musicBot/musicBot.py
@@ -303,6 +303,63 @@ async def problem(ctx):
             await ctx.send(f"Your current balance is {user_balances[user_id]}.")
         else:
             await ctx.send("you are a degenerate, get some help")
+            
+@bot.command(name='slots', help='Play the slot machine')
+async def slots(ctx):
+    user_id = str(ctx.author.id)
+    if user_id not in user_balances:
+        update_balance(user_id, 0)
+
+    bet = 5
+
+    if bet > user_balances[user_id]:
+        await ctx.send("You're too poor to play.")
+        return
+
+    update_balance(user_id, -bet)
+
+    slots_symbols = ["🍒", "🍋", "🔔", "💎", "7️⃣"]
+    grid = [random.choices(slots_symbols, k=3) for _ in range(3)]
+    for row in grid:
+        result = " | ".join(row)
+        msg = await ctx.send(result)
+        await asyncio.sleep(0.5)
+
+    win_amount = 0
+    for row in grid:
+        if len(set(row)) == 1:
+            symbol = row[0]
+            if symbol == "🍒":
+                win_amount += 5
+            elif symbol == "🍋":
+                win_amount += 10
+            elif symbol == "7️⃣":
+                win_amount += 15
+            elif symbol == "🔔":
+                win_amount += 20
+            elif symbol == "💎":
+                win_amount += 100
+
+
+    if grid[0][0] == grid[1][1] == grid[2][2] or grid[0][2] == grid[1][1] == grid[2][0]:
+        symbol = grid[1][1]
+        if symbol == "🍒":
+            win_amount += 2
+        elif symbol == "🍋":
+            win_amount += 4
+        elif symbol == "🔔":
+            win_amount += 8
+
+    if all(symbol == "💎" for row in grid for symbol in row):
+        win_amount = 10000000
+
+    if win_amount > 0:
+        await ctx.send(f"🎉 You won ${win_amount}!")
+        update_balance(user_id, win_amount)
+    else:
+        await ctx.send("Sorry, you didn't win anything. Better luck next time.")
+
+    await ctx.send(f"Your new balance is: {user_balances[user_id]}.")
 
 if __name__ == "__main__" :
     load_balances()
