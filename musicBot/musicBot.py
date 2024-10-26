3bb1e9baab2bbd21366f0634922d6bf78459ae5c
Priscilla Kang <64219410+priscillakang@users.noreply.github.com>
2024-10-26 02:29:05 -0400

    Update musicBot.py
    Removed where the heck is saki listener because Jackson hated it

--- musicBot/musicBot.py
+++ musicBot/musicBot.py
@@ -532,14 +532,6 @@ async def ball(ctx, msg=None):
     selectedStatement = random.choice(responses)
     await ctx.send(selectedStatement)
 
-@bot.event
-async def on_message(message):
-    if message.author == client.user:
-        return
-
-    if message.content.startswith('Where the heck'):
-        await message.channel.send('She\'s waiting down in the lobby')
-    
 @bot.command(name='test_button', help='test button')
 async def test_button(ctx):
     view = ButtonView()
